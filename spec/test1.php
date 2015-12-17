d7f2b1a8211b5771088e57fe3248167f3af99868
Clayton Carter <claytonrcarter@gmail.com>
2015-12-16 22:41:48 -0500

    basic support for jumping to function declarations

--- spec/test1.php
+++ spec/test1.php
@@ -10,6 +10,13 @@ $var1 = "declare";
 
 $var2 = "I do " + $var1 + "!";
 
+function this_is_a_function($param1, $param2) {
+  return $param1;
+}
+function also_a_func() { return; }
+
 echo $var2;
 echo "$var2";
 echo $var_from_other_file;
+
+$v =  this_is_a_function( $var1, $var2, also_a_func() );
